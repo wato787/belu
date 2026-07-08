b251bc2e173e2284fb1df18758c5e1a49b70dce6
wato787 <wato787@icloud.com>
2026-07-08 21:17:51 +0900

    feat: Buttonコンポーネントとスタイルを新規追加

---
+++ apps/web/src/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import { Button as BaseButton } from "@base-ui-components/react/button";
+import type { ButtonProps as BaseButtonProps } from "@base-ui-components/react/button";
+import type { ButtonHTMLAttributes } from "react";
+
+import { cx } from "../../utils/cx";
+import styles from "./Button.module.css";
+
+type NativeButtonType = ButtonHTMLAttributes<HTMLButtonElement>["type"];
+
+type ButtonProps = Omit<BaseButtonProps, "className" | "nativeButton" | "type"> & {
+  className?: string;
+  fullWidth?: boolean;
+  isLoading?: boolean;
+  type?: NativeButtonType;
+};
+
+export const Button = ({
+  children,
+  className,
+  disabled,
+  fullWidth = false,
+  isLoading = false,
+  type = "button",
+  ...props
+}: ButtonProps) => (
+  <BaseButton
+    className={cx(styles.button, className)}
+    data-full-width={fullWidth}
+    disabled={disabled || isLoading}
+    nativeButton
+    type={type}
+    {...props}
+  >
+    {isLoading ? <span className={styles.spinner} /> : children}
+  </BaseButton>
+);
