e417a2aeb45f3b105662eeb62028836d95a8785f
wato787 <wato787@icloud.com>
2026-07-08 21:16:57 +0900

    feat: Inputコンポーネントとスタイルを新規追加

---
+++ apps/web/src/components/Input/Input.tsx
@@ -0,0 +1,13 @@
+import { Input as BaseInput } from "@base-ui-components/react/input";
+import type { ComponentPropsWithoutRef } from "react";
+
+import { cx } from "../../utils/cx";
+import styles from "./Input.module.css";
+
+type InputProps = Omit<ComponentPropsWithoutRef<typeof BaseInput>, "className"> & {
+  className?: string;
+};
+
+export const Input = ({ className, ...props }: InputProps) => (
+  <BaseInput className={cx(styles.input, className)} {...props} />
+);
