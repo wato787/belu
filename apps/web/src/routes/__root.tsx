425eab2fee7020fb60470d5c1c49becd7fc2af36
wato787 <wato787@icloud.com>
2026-07-10 16:42:12 +0900

    feat: 成功メッセージ用のスタイルを追加し、ログインエラーメッセージを改善

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -10,7 +10,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <Outlet />
-      <Toaster position="top-right" />
+      <Toaster closeButton position="top-right" richColors />
     </>
   ),
 });
