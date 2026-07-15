f680269fc300dbf0185c5cdc7d195eb243099fb3
wato787 <wato787@icloud.com>
2026-07-15 22:24:59 +0900

    feat: スペースメンバーのルートに招待リストの取得を追加

--- apps/web/src/routes/_authenticated/spaces_.$spaceId.members.tsx
+++ apps/web/src/routes/_authenticated/spaces_.$spaceId.members.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { invitesQueries } from "../../features/invites";
 import { SpaceMembers } from "../../features/members";
+import { membersQueries } from "../../features/members";
 
 const SpaceMembersRoute = () => {
   const { spaceId } = Route.useParams();
@@ -9,5 +11,10 @@ const SpaceMembersRoute = () => {
 };
 
 export const Route = createFileRoute("/_authenticated/spaces_/$spaceId/members")({
+  loader: ({ context, params }) =>
+    Promise.all([
+      context.queryClient.ensureQueryData(membersQueries.list(params.spaceId)),
+      context.queryClient.ensureQueryData(invitesQueries.list(params.spaceId)),
+    ]),
   component: SpaceMembersRoute,
 });
